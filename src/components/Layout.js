a2602e3030ff73260ef08dc0091529ca4ac14d77
7jcdev <7jaydev@protonmail.com>
2024-11-23 20:41:41 -0600

    Fixed title

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,12 @@
 import Head from "next/head";
 
 const Layout = ({children, pagina}) => {
+
+    
     return (
         <div>
             <Head>
-                <title> Shork - {pagina} </title>
+                <title>{`Shork - ${pagina}`}</title>
             </Head>
             <div className="py-20 mx-auto xl:w-[72rem]">
                 {children}
