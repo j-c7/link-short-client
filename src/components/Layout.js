8e1053073c8fa4ae5c8f030d57d30705717a43f5
7jcdev <7jaydev@protonmail.com>
2024-11-28 15:52:44 -0600

    Added background gradient

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,11 @@
 import Head from "next/head";
 
-const Layout = ({children, pagina}) => {
+const Layout = ({ children, pagina }) => {
 
-    
+
     return (
-        <div>
+        <div class="relative">
+            <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(8,145,178,0.2),rgba(255,255,255,0))]"></div>
             <Head>
                 <title>{`Shork - ${pagina}`}</title>
             </Head>
